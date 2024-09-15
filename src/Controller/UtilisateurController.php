e23dd94eb447ed12998b18d8c7161f2ad7f2b930
loyet <thomas.loye@etu.umontpellier.fr>
2024-09-15 18:05:30 +0200

    feat : début twig et front-end. Premier controller et installation des dépendances Symfony

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Publication;
+use App\Form\PublicationType;
+use App\Service\FlashMessageHelper;
+use App\Service\FlashMessageHelperInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PublicationRepository;
+
+class UtilisateurController extends AbstractController
+{
+
+    #[Route('/', name: 'pages_rouges')]
+    public function pagesRouges(Request $request): Response
+    {
+        return $this->render('utilisateurs/accueil.html.twig');
+    }
+}
