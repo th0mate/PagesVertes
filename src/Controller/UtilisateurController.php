a346236df546e3516088913255f17a9c31a17a8f
loyet <thomas.loye@etu.umontpellier.fr>
2024-09-17 12:57:15 +0200

    feat : Services

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -20,6 +20,37 @@ class UtilisateurController extends AbstractController
     public function pagesRouges(Request $request): Response
     {
         $this->addFlash('success', 'ceci est un test');
-        return $this->render('utilisateurs/accueil.html.twig');
+        return $this->render('utilisateurs/accueil.html.twig', ['page_actuelle' => 'Accueil']);
     }
+
+    #[Route('/credits', name: 'credits', methods: 'GET')]
+    public function afficherCredits(): Response
+    {
+        return $this->render('credits/credits.html.twig', ['page_actuelle' => 'Credits']);
+    }
+
+    #[Route('/inscription', name: 'inscription', methods: ['GET', 'POST'])]
+    public function inscription(Request $request, EntityManagerInterface $entityManager): Response
+    {
+
+        if($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('feed');
+        }
+
+        $form = $this->createForm(UtilisateurType::class, new Utilisateur(), ['method' => 'POST', 'action' => $this->generateUrl('inscription')]);
+        $form->handleRequest($request);
+        $this->flashMessageHelperInterface->addFormErrorsAsFlash($form);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $utilisateur = $form->getData();
+            $this->utilisateurManagerInterface->processNewUtilisateur($utilisateur, $form->get('plainPassword')->getData(), $form->get('fichierPhotoProfil')->getData());
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+            $this->addFlash('success', 'Inscription réussie : bienvenue sur The Feed !');
+            return $this->redirectToRoute('feed');
+        }
+
+        return $this->render('utilisateur/inscription.html.twig', ['formInscription' => $form]);
+    }
+
 }
