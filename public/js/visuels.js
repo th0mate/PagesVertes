eaa21d105ceb4f7db4e97bac75a5e4512079f7d1
loyet <thomas.loye@etu.umontpellier.fr>
2024-09-17 17:30:36 +0200

    feat : bandeau responsive

--- public/js/visuels.js
+++ public/js/visuels.js
@@ -3,4 +3,13 @@
  */
 function afficherMenu() {
     document.querySelector(".bandeau").style.display = "flex";
+    document.querySelector(".close").style.display = "block";
+}
+
+/**
+ * Cache le bandeau sur l'écran après un click sur la croix dans le bandeau
+ */
+function cacherMenu() {
+    document.querySelector(".bandeau").style.display = "none";
+    document.querySelector(".close").style.display = "none";
 }
