3126a605192f845571acf74fe6f479c7a73dd544
loyet <thomas.loye@etu.umontpellier.fr>
2024-09-19 12:53:36 +0200

    fix : réparation BD

--- migrations/Version20240919105048.php
+++ migrations/Version20240919105048.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240918205552 extends AbstractMigration
+final class Version20240919105048 extends AbstractMigration
 {
     public function getDescription(): string
     {
